ad7bee2386d23ac8af00e76331a32a3baff592d7
ScottKo701223 <118521086+ScottKo701223@users.noreply.github.com>
2024-06-28 14:59:06 +0800

    Update web.php

--- php/routes/web.php
+++ php/routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
-
+use App\Http\Controllers\LineHook\LineGPT;
 use Illuminate\Http\Request;
 
-
+Route::post('/linegpt', function (Request $request) {
+  if(isset($request['events'][0])){
+    $LineHook = new LineGPT($request,"accessKey","secret");
+    $LineHook->IncommingMsgProccess();
+  }
+  return response("{\"status\":0}",200);
+});
 
 Route::get('{path?}',function(Request $request,$path=null){
   if($path!="dl")return view('iotPortal');
